f4f3e0aae8ffdab8ec4375c1d179ce6b1ec43b86
r00cha <roocha99pt@gmail.com>
2023-05-17 12:35:53 +0100

    Style titles, new ticket form, and tags searchbar

--- server/templates/ticketCreator.tpl.php
+++ server/templates/ticketCreator.tpl.php
@@ -1,49 +1,56 @@
 <?php 
 function drawTicketCreator($departments) { ?>
-
-    <article class="form" id="create-ticket">
-      <h1 class="title">Create a New Ticket</h1>
-      <form action="../actions/createTicket.action.php" method="post">
-        <section class="input-container ic1">
-          <input id="subject" class="input" type="text" placeholder=" " name="subject" />
-          <article class="cut"></article>
-          <label for="subject" class="placeholder">Subject</label>
-        </section>
-        <section class="input-container ic2">
-          <textarea id="description" class="input" placeholder=" " name="description"></textarea>
-          <article class="cut"></article>
-          <label for="description" class="placeholder">Description</label>
-        </section>
-        <section class="input-container ic2">
-          <select id="priority" class="input" name="priority">
-            <option value="Normal">Normal</option>
-            <option value="Urgent">Urgent</option>
-            <option value="Immediate">Immediate</option>
-          </select>
-          <article class="cut"></article>
-          <label for="priority" class="placeholder">Priority</label>
-        </section>
-        <section class="input-container ic2">
-          <!---<input type="text" id="tag-input" class="input" name="tag" placeholder=" "/>
-          <article class="cut"></article>
-          <label for="tag-input" class="placeholder">Tags</label>-->
-          <ul class="tags-box tags" id="tag-creator">
-						<input type="text" id="tag-input" name="tag" placeholder="Tags">
-					</ul>
-					<input type="hidden" id="tags" name="tags" value="">
-        </section>
-        <section class="input-container ic2">
-          <select id="department" class="input" name="department">
-            <option value="-1">None</option>
-            <?php foreach ($departments as $department) { ?>
-              <option value="<?= $department->id; ?>"><?= $department->name; ?></option>
-            <?php } ?>
-          </select>
-          <article class="cut"></article>
-          <label for="department" class="placeholder">Department</label>
-        </section>
-        <button type="text" class="submit" id="submit-button">Submit</button>
-      </form>
-    </article>
+    <main class="middle-column">
+      <section class="title">
+      <h2 class="title">Create a New Ticket</h2>
+      </section>
+      <section>
+        <article class="form" id="create-ticket">
+          <h1 class="title">Create a New Ticket</h1>
+          <form action="../actions/createTicket.action.php" method="post">
+            <section class="input-container ic1">
+              <input id="subject" class="input" type="text" placeholder=" " name="subject" />
+              <article class="cut"></article>
+              <label for="subject" class="placeholder">Subject</label>
+            </section>
+            <section class="input-container ic2">
+              <textarea id="description" class="input" placeholder=" " name="description"></textarea>
+              <article class="cut"></article>
+              <label for="description" class="placeholder">Description</label>
+            </section>
+            <section class="input-container ic2">
+              <select id="priority" class="input" name="priority">
+                <option value="Normal">Normal</option>
+                <option value="Urgent">Urgent</option>
+                <option value="Immediate">Immediate</option>
+              </select>
+              <article class="cut"></article>
+              <label for="priority" class="placeholder">Priority</label>
+            </section>
+      
+            <section class="input-container ic2">
+              <select id="department" class="input" name="department">
+                <option value="-1">None</option>
+                <?php foreach ($departments as $department) { ?>
+                  <option value="<?= $department->id; ?>"><?= $department->name; ?></option>
+                <?php } ?>
+              </select>
+              <article class="cut"></article>
+              <label for="department" class="placeholder">Department</label>
+            </section>
+            <section class="input-container ic2">
+              <!---<input type="text" id="tag-input" class="input" name="tag" placeholder=" "/>
+              <article class="cut"></article>
+              <label for="tag-input" class="placeholder">Tags</label>-->
+              <ul class="tags-box tags" id="tag-creator">
+                <input type="text" id="tag-input" name="tag" placeholder="Tags">
+              </ul>
+              <input type="hidden" id="tags" name="tags" value="">
+            </section>
+            <button type="text" class="submit" id="submit-button">Submit</button>
+          </form>
+        </article>
+      </section>
+    </main>
 
 <?php } ?>
