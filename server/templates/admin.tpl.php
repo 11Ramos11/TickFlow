28f8276d835a4533b07a706d565b37a576346bac
migueljcpedrosa <migueljcpedrosa@gmail.com>
2023-05-20 01:02:35 +0100

    Submit button now on top of cancel button

--- server/templates/admin.tpl.php
+++ server/templates/admin.tpl.php
@@ -10,8 +10,8 @@
                 <form action=<?=$removeAction?> method="post">
                     <input type="hidden" name="id" value=<?=$item->id?>>
                     <p>Do you want to remove the <?=$itemType?> <?=$item->name?>?<p>
-                    <button type="button" class="cancel-button">no</button>
                     <button type="submit">yes</button>
+                    <button type="button" class="cancel-button">no</button>
                 </form>
             </dialog>
             <dialog class="edit-dialog">
@@ -19,8 +19,8 @@
                     <input type="hidden" name="id" value=<?=$item->id?>>
                     <label for="name">Change <?=$item->name?> name to:</label>
                     <input type="text" name="name" id="name" required>
-                    <button type="button" class="cancel-button">cancel</button>
                     <button type="submit">submit</button>
+                    <button type="button" class="cancel-button">cancel</button>
                 </form>
             </dialog>
         </li>
@@ -50,8 +50,8 @@
                     <form action="../actions/createDepartment.action.php" method="post">
                         <label for="name">New Department Name</label>
                         <input type="text" name="name" id="name" required>
-                        <button type="button" class="cancel-button">Cancel</button>
                         <button type="submit">Add</button>
+                        <button type="button" class="cancel-button">Cancel</button>
                     </form>
                 </dialog>
             </article>
@@ -72,8 +72,8 @@
                     <form action="../actions/createStatus.action.php" method="post">
                         <label for="name">New Status Name</label>
                         <input type="text" name="name" id="name" required>
-                        <button type="button" class="cancel-button">Cancel</button>
                         <button type="submit">Add</button>
+                        <button type="button" class="cancel-button">Cancel</button>
                     </form>
                 </dialog>
             </article>
@@ -94,8 +94,8 @@
                     <form action="../actions/createPriority.action.php" method="post">
                         <label for="name">New Priority Name</label>
                         <input type="text" name="name" id="name" required>
-                        <button type="button" class="cancel-button">Cancel</button>
                         <button type="submit">Add</button>
+                        <button type="button" class="cancel-button">Cancel</button>
                     </form>
                 </dialog>
             </article>
