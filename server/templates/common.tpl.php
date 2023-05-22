6a7f07298aa5bcade618c553035e15e5f1aa63a8
Ramos <1joaoramos1@gmail.com>
2023-05-22 09:47:10 +0100

    fixed responsiveness phone

--- server/templates/common.tpl.php
+++ server/templates/common.tpl.php
@@ -29,6 +29,7 @@
 
 <head>
 	<title>TickFlow</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="../styles/<?=$style?>.css">
 	<link rel="stylesheet" type="text/css" href="../styles/snackbar.css">
 	<script src="https://kit.fontawesome.com/a45efa4a81.js" crossorigin="anonymous"></script>
